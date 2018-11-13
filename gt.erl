314f3d657b4fdfd9869b31f43a44adc7a062798c
prashanth-g <prashanthcsgce@gmail.com>
2018-11-13 07:59:28 +0530

    Added atoms

--- gt.erl
+++ gt.erl
@@ -1,5 +1,5 @@
 -module(gt). % defing module
--export([fac/1, square/1]). % setting the function square for outside access
+-export([fac/1, square/1, convert/2]). % setting the function square for outside access
 
 % functions
 square(X) ->
@@ -9,3 +9,9 @@ fac(1) ->
     1;
 fac(N) ->
     N * fac(N - 1).
+
+convert(M, inch) ->
+    M / 2.54;
+
+convert(M, centimeter) ->
+    M * 2.54.
